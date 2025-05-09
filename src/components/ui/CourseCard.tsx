a063d5b26a63e71f1cf577462a120b1a33c27cfe
Fernando <crespofernando05@gmail.com>
2025-05-09 19:28:10 -0400

    switch to next Image tag

--- src/components/ui/CourseCard.tsx
+++ src/components/ui/CourseCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type CourseCardProps = {
     name: string;
     code: string;
@@ -7,7 +9,7 @@ type CourseCardProps = {
   export default function CourseCard({ name, code, imageUrl }: CourseCardProps) {
     return (
       <div className="bg-white rounded-2xl shadow-md overflow-hidden cursor-pointer">
-        <img src={imageUrl} alt={name} className="w-full h-40 object-cover" />
+        <Image src={imageUrl} alt={name} className="w-full h-40 object-cover" />
         <div className="p-4">
           <h3 className="text-lg font-semibold">{name}</h3>
           <p className="text-gray-500">{code}</p>
