c6ccd37662268bc401a2c0d66371b1d6fbd0cf5c
Fernando <crespofernando05@gmail.com>
2025-05-08 23:59:27 -0400

    finish request course procedure

--- src/server/api/routers/course.ts
+++ src/server/api/routers/course.ts
@@ -1,3 +1,4 @@
+import { Code } from "lucide-react";
 import { z } from "zod";
 
 import {
@@ -7,6 +8,37 @@ import {
 } from "~/server/api/trpc";
 
 export const courseRouter = createTRPCRouter({
-    
+    requestCourse: protectedProcedure
+    .input(z.object({
+        name: z.string(),
+        prefix: z.string(),
+        code: z.string(),
+    })).mutation(async ({ctx, input}) => {
+        const fullCode = input.prefix + input.code;
+        const course = await ctx.db.course.findUnique({
+            where: {
+                code: fullCode,
+            }
+        })
+
+        if(course === null) {
+            return ctx.db.course.create({
+                data: {
+                    name: input.name,
+                    code: fullCode,
+                }
+            })
+        } else {
+            return ctx.db.course.update({
+                where: {
+                    code: fullCode,
+                },
+                data: {
+                    count: course.count + 1,
+                },
+            })
+        }
+
+    }),
 
 });
