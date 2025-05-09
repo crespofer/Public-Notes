4747144f46b5febb238ff4f7c56859a5d16582f2
Fernando <crespofernando05@gmail.com>
2025-05-08 23:28:13 -0400

    set up initial course router

---
+++ src/server/api/routers/course.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const courseRouter = createTRPCRouter({
+    
+
+});
