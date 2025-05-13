e65b229753e8e65e9943d156b89e0ad119bf066e
Fernando <crespofernando05@gmail.com>
2025-05-13 00:33:46 -0400

    create get all courses procedure

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -6,5 +6,9 @@ import {
 } from "~/server/api/trpc";
 
 export const adminRouter = createTRPCRouter({
-    
+    getAllCourses: adminProcedure
+    .query(async ({ ctx }) => {
+        const courses = await ctx.db.course.findMany();
+        return courses;
+    })
 })
