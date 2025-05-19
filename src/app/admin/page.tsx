38877740a3ae22bcc2a333ace751131334786122
Fernando <crespofernando05@gmail.com>
2025-05-18 23:14:07 -0400

    add tabs for tables

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,6 +2,8 @@ import { api } from "~/trpc/server";
 import ApprovedCourseTable from "./ApprovedCourseTable";
 import PendingCourseTable from "./PendingCourseTable";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
+
 export default async function AdminDashboard() {
 
   const pendingCourses = await api.admin.getAllPendingCourses();
@@ -11,9 +13,19 @@ export default async function AdminDashboard() {
     <main className="py- mx-auto max-w-6xl px-4 pt-15">
       <h1 className="mb-6 text-2xl font-bold">Admin Dashboard</h1>
 
-      <ApprovedCourseTable courses={approvedCourses} />
-      <PendingCourseTable courses={pendingCourses}/>
-      
+      <Tabs defaultValue="pending" className="w-full">
+        <TabsList>
+          <TabsTrigger className="cursor-pointer" value="pending">Pending Courses</TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="approved">ApprovedCourses</TabsTrigger>
+        </TabsList>
+        <TabsContent value="pending">
+          <PendingCourseTable courses={pendingCourses}/>
+        </TabsContent>
+        <TabsContent value="approved">
+          <ApprovedCourseTable courses={approvedCourses} />
+        </TabsContent>
+      </Tabs>
+
     </main>
   );
 }
