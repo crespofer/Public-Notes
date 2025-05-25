b9a9bd869c544fb8f8d68b6de890915d4cd71d85
Fernando <crespofernando05@gmail.com>
2025-05-24 23:51:49 -0400

    remove mock data display fetched courses

--- src/components/ui/CourseCard.tsx
+++ src/components/ui/CourseCard.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 type CourseCardProps = {
     name: string;
     code: string;
-    imageUrl: string;
+    imageUrl: string | null;
   };
   
   export default function CourseCard({ name, code, imageUrl }: CourseCardProps) {
     return (
       <div className="bg-white rounded-2xl shadow-md overflow-hidden cursor-pointer">
-        <img src={imageUrl} alt={name} className="w-full h-40 object-cover" />
+        <img src={imageUrl ?? ""} alt={name} className="w-full h-40 object-cover" />
         <div className="p-4">
           <h3 className="text-lg font-semibold">{name}</h3>
           <p className="text-gray-500">{code}</p>
