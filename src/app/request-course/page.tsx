7c266c893a65b526030ac72893f0a446db23a0f5
Fernando <crespofernando05@gmail.com>
2025-05-30 17:55:49 -0400

    authenticate routes

--- src/app/request-course/page.tsx
+++ src/app/request-course/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "next/navigation";
 import RequestCourseForm from "~/components/RequestCourseForm";
+import { auth } from "~/server/auth";
 
-export default function RequestCourses() {
+export default async function RequestCourses() {
+  const session = await auth();
+
+  if(!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
     <main className="mx-auto max-w-7xl px-4 py-8 pt-15">
       <h1 className="mb-6 text-3xl font-bold">Request to add a Course</h1>
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,7 +1,14 @@
+import { redirect } from "next/navigation";
 import UploadNoteForm from "~/components/UploadNoteForm";
+import { auth } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function UploadPage() {
+  const session = await auth();
+  if(!session) {
+    redirect("/api/auth/signin");
+  }
+  
   const courses = await api.course.fetchCourses();
 
   return (
