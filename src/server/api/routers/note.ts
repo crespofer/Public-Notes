77ee045aec27ac112566459f707d2bceed8a4dd2
Fernando <crespofernando05@gmail.com>
2025-05-30 19:34:06 -0400

    hash file names for unique names

--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -3,6 +3,9 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import crypto from "crypto";
+
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex");
 
 const s3 = new S3Client({
     region: process.env.AWS_BUCKET_REGION!,
@@ -34,7 +37,7 @@ export const noteRouter = createTRPCRouter({
         // all this gets embedded into the url
         const putObjectCommand = new PutObjectCommand({
             Bucket: process.env.AWS_BUCKET_NAME!,
-            Key: "test-file",
+            Key: generateFileName(),
             ContentType: input.type,
             ContentLength: input.size,
             ChecksumSHA256: input.checksum,
